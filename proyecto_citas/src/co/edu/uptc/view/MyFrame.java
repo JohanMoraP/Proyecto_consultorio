4df57c7dbf0b820b4458fbc72d743e86048c66fd
Johan Mora <johan.mora@uptc.edu.co>
2023-05-13 18:28:03 -0500

    update

--- proyecto_citas/src/co/edu/uptc/view/MyFrame.java
+++ proyecto_citas/src/co/edu/uptc/view/MyFrame.java
@@ -2,23 +2,26 @@ package co.edu.uptc.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
+
 	public MyFrame() {
 
 		super("Consultorio");
 		this.setExtendedState(MAXIMIZED_BOTH);
+		Image icon = Toolkit.getDefaultToolkit().getImage("image/icono_Consultorio.png");
+		setIconImage(icon);
 		this.setSize(1000, 500);
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBackground(new Color(247, 249, 252));
 		initComponents();
 		this.setVisible(true);
 		this.revalidate();
 		this.pack();
-
 	}
 
 	public void initComponents() {
