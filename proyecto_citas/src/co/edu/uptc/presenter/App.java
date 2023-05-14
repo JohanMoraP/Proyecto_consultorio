77eb3548e92c6d58b4fa56982ed4a05fe2badf91
Johan Mora <johan.mora@uptc.edu.co>
2023-05-14 12:48:17 -0500

    update App.java

---
+++ proyecto_citas/src/co/edu/uptc/presenter/App.java
@@ -0,0 +1,17 @@
+package co.edu.uptc.presenter;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class App implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new App();
+    }
+}
